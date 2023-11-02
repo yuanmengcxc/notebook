3478cc4744d65d97d5752bb6abad721067b0a77c
Yuan <120163630+yuanmengcxc@users.noreply.github.com>
2023-11-02 17:16:35 +0800

    Add files via upload

---
+++ 单双链表及其反转.cpp
@@ -0,0 +1,52 @@
+﻿#include<iostream>
+using namespace std;
+
+//单链表结构
+class ListNode {
+public:
+
+	int val;
+	ListNode* next;
+
+	ListNode(int v) :val(v), next(nullptr) {};
+	ListNode(int v, ListNode* n) :val(v), next(n) {};
+};
+
+//双链表结构
+class DoubleListNode {
+public:
+	int val;
+	DoubleListNode* next;//指向后继地址
+	DoubleListNode* last;//指向前驱地址
+
+	DoubleListNode(int v) :val(v), next(nullptr), last(nullptr) {};
+};
+
+//反转单链表
+ListNode* reverseList(ListNode* head) {
+	ListNode* pre = nullptr;
+	ListNode* next = nullptr;
+	while (head != nullptr) {
+		next = head->next;
+		head->next = pre;
+		pre = head;
+		head = next;
+	}
+	return pre;
+}
+
+DoubleListNode* reverseDoubleList(DoubleListNode* head) {
+	DoubleListNode* pre = nullptr;
+	DoubleListNode* next = nullptr;
+	while (head != nullptr) {
+		next = head->next;
+		head->next = pre;
+		head->last = next;
+		pre = head;
+		head = next;
+	}
+	return pre;
+}
+int main() {
+	return 0;
+}
